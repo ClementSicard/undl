12b2a4cc553d16fe917462ee512845193b4b49a5
Clément Sicard <c.clement.sicard@northvolt.com>
2023-06-25 14:43:59 +0200

    main.py: new args

--- main.py
+++ main.py
@@ -1,33 +1,70 @@
 import argparse
+import json
 from typing import Any, Dict
+
+from loguru import logger
 
 from undl.client import UNDLClient
 
 
 def main(args: Dict[str, Any]):
+    """
+    Runs a query against the UN Digital Library API.
+
+    Parameters
+    ----------
+    `args` : `Dict[str, Any]`
+        Dictionary of CLI arguments passed to the script.
+    """
     client = UNDLClient(verbose=args["verbose"])
-    client.query(
-        query=args["query"],
-        output_format=args["format"],
-        n_results=args["n_results"],
-        output_file=args["output"],
-    )
+
+    if args["id"]:
+        client.queryById(
+            recordId=args["id"],
+            outputFormat=args["format"],
+            outputFile=args["output"],
+        )
+    else:
+        client.query(
+            query=args["query"],
+            outputFormat=args["format"],
+            nbResults=args["n_results"],
+            outputFile=args["output"],
+        )
 
 
 def parse_args() -> Dict[str, Any]:
+    """
+    Parse CLI arguments.
+
+    Returns
+    -------
+    `Dict[str, Any]`
+        Dictionary of CLI arguments passed to the script.
+    """
     parser = argparse.ArgumentParser(
         prog="main.py",
         description="Query the UN Digital Library API with a Python wrapper.",
     )
     parser.add_argument(
-        "query",
+        "-q",
+        "--query",
         help="Query string to search for.",
+    )
+    parser.add_argument(
+        "--id",
+        help="ID of the record to search for.",
     )
     parser.add_argument(
         "-v",
         "--verbose",
         action="store_true",
         help="Print verbose output.",
+    )
+    parser.add_argument(
+        "--remove-empty",
+        action="store_true",
+        help="Removes empty field from output JSON.",
     )
     parser.add_argument(
         "-o",
@@ -56,4 +93,5 @@ def parse_args() -> Dict[str, Any]:
 
 if __name__ == "__main__":
     args = parse_args()
+    logger.debug(f"Args:\n{json.dumps(args, indent=2)}")
     main(args=args)
